0780a4d84b431fb3cbeb1e1dbfff761d4ec442a0
AntistesEM <mev.86@list.ru>
2024-05-31 12:08:11 +0300

    modified css

--- src/components/UiFC/UiFC.jsx
+++ src/components/UiFC/UiFC.jsx
@@ -5,15 +5,16 @@ export const UiFC = (props) => {
   
   return (
     <div className={clases["main-content"]}>
-      <h2 className={clases["h2"]}>{brand}</h2>
-      <h1 className={clases["h1"]}>{title}</h1>
-      <h3 className={clases["h3"]}>{description}</h3>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
       <div className={clases["description"]}>{descriptionFull}</div>
-      <div className={clases["highlight-window mobile"]}><div className={clases["highlight-overlay"]}></div></div>
+      <div className={clases["highlight-window mobile"]}>
+        <div className={clases["highlight-overlay"]}></div></div>
       <div className={clases["divider"]}></div>
       <div className={clases["purchase-info"]}>
         <div className={clases["price"]}>{currency}{price.toFixed(2)}</div>
-        <button className={clases["button"]}>Добавить в корзину</button>
+        <button>Добавить в корзину</button>
       </div>
     </div>
   )
