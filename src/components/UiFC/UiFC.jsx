1cdcc6fe56218c5f445caec4804a17ee1fc4a326
AntistesEM <mev.86@list.ru>
2024-05-31 12:20:31 +0300

    css как в задании

--- src/components/UiFC/UiFC.jsx
+++ src/components/UiFC/UiFC.jsx
@@ -1,19 +1,19 @@
-import clases from "./UiFC.module.css";
+// import clases from "./UiFC.module.css";
 
 export const UiFC = (props) => {
   const { brand, title, description, descriptionFull, price, currency } = props.item;
   
   return (
-    <div className={clases["main-content"]}>
+    <div className="main-content">
       <h2>{brand}</h2>
       <h1>{title}</h1>
       <h3>{description}</h3>
-      <div className={clases["description"]}>{descriptionFull}</div>
-      <div className={clases["highlight-window mobile"]}>
-        <div className={clases["highlight-overlay"]}></div></div>
-      <div className={clases["divider"]}></div>
-      <div className={clases["purchase-info"]}>
-        <div className={clases["price"]}>{currency}{price.toFixed(2)}</div>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
